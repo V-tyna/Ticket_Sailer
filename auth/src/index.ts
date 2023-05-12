88e0cdc0c7ce7b49f69405cce2a0fd8ad9027e1e
AnnaAntypova <anna.antipova794@gmail.com>
2023-05-12 20:25:14 +0300

    Password hashing. JWT-Cookie-session mechanism in progress.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -9,9 +9,17 @@ import currentUserRouter from './routes/current-user';
 import signInRouter from './routes/signin';
 import signOutRouter from './routes/signout';
 import signUpRouter from './routes/signup';
+import cookieSession from 'cookie-session';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signInRouter);
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,14 +1,16 @@
 import { Router, Request, Response } from 'express';
 import { validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { signUpValidators } from '../utils/route-validators';
+import { User } from '../models/mongoose/user';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const signUpRouter = Router();
 
 signUpRouter.post('/api/users/signup', signUpValidators,
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -17,11 +19,25 @@ signUpRouter.post('/api/users/signup', signUpValidators,
 
     const { email, password } = req.body;
 
-    console.log('User created..');
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    // new User({ email, password });
-    // res.send({});
+    if (existingUser) {
+      throw new BadRequestError('Email in use.');
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, 'Temporary secret');
+
+    req.session = {
+      jwt: userJwt
+    };
+
+    res.status(201).send(user);
   });
 
 export default signUpRouter;
